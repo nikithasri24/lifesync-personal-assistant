fed3173fb5deb9057ccec6949d756398684cd86f
Sri Nikitha Kalidindi <sri.nikitha@Sris-MacBook-Pro.local>
2026-01-06 19:47:56 -0800

    Remove obsolete Supabase migration files

--- supabase/migrations/20251228000001_drop_scheduled_time_from_tasks.sql
+++
@@ -1,4 +0,0 @@
--- Remove legacy scheduled_time column now that scheduled_start/scheduled_end are canonical
-
-ALTER TABLE tasks
-DROP COLUMN IF EXISTS scheduled_time;
--- supabase/migrations/20251231000001_drop_period_cycles.sql
+++
@@ -1,3 +0,0 @@
--- Remove period tracking table (period module not supported)
-
-DROP TABLE IF EXISTS period_cycles;
